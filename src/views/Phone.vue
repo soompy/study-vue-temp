c4b96f4e53b651f1fd708ecdf6efeeb1224675a2
soompy <sooper1137@gmail.com>
2020-02-09 22:12:46 +0900

    애플 홈페이지 VUE로 구현하기

--- src/views/Phone.vue
+++ src/views/Phone.vue
@@ -3,7 +3,7 @@
 
         <div class="position-relative">
             <!-- shape Hero -->
-            <section class="section-shaped my-0">
+            <section class="section-shaped my-0 visual">
                 <div class="shape shape-style-1 shape-default shape-skew">
                     <span></span>
                     <span></span>
@@ -18,27 +18,25 @@
                 <div class="container shape-container d-flex">
                     <div class="col px-0">
                         <div class="row">
-                            <div class="col-lg-6">
-                                <h1 class="display-3  text-white">폰
-                                    <span>completed with examples</span>
+                            <div class="col-sm-12">
+                                <h1 class="display-3  text-white">iPhone 11 Pro
+                                    <span>프로급 카메라. 프로급 디스플레이. 프로급 성능.</span>
                                 </h1>
-                                <p class="lead  text-white">The design system comes with four pre-built pages to help
-                                    you get started faster. You can change the text and images and you're good to
-                                    go.</p>
+                                <p class="lead  text-white">보상 판매 시 ₩1,110,000부터.*</p>
                                 <div class="btn-wrapper">
                                     <base-button tag="a"
-                                                 href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
+                                                 href="https://www.apple.com/kr-k12/shop/buy-iphone/iphone-11-pro"
                                                  class="mb-3 mb-sm-0"
-                                                 type="info"
-                                                 icon="fa fa-code">
-                                        Components
+                                                 type="primary"
+                                                 >
+                                        구입하기
                                     </base-button>
                                     <base-button tag="a"
-                                                 href="https://www.creative-tim.com/product/argon-design-system"
+                                                 href="https://www.apple.com/kr/iphone-11-pro/"
                                                  class="mb-3 mb-sm-0"
                                                  type="white"
-                                                 icon="ni ni-cloud-download-95">
-                                        Download HTML
+                                                 icon="">
+                                        더 알아보기 &gt;
                                     </base-button>
                                 </div>
                             </div>
@@ -57,7 +55,7 @@
                                 <card class="border-0" hover shadow body-classes="py-5">
                                     <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
                                     </icon>
-                                    <h6 class="text-primary text-uppercase">Download Argon</h6>
+                                    <h6 class="text-primary text-uppercase">iPhone 11 Pro 가죽케이스</h6>
                                     <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
                                         that includes the most important components and features.</p>
                                     <div>
@@ -74,7 +72,7 @@
                                 <card class="border-0" hover shadow body-classes="py-5">
                                     <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
                                     </icon>
-                                    <h6 class="text-success text-uppercase">Build Something</h6>
+                                    <h6 class="text-success text-uppercase">iPhone 11 실리콘 케이스</h6>
                                     <p class="description mt-3">Argon is a great free UI package based on Bootstrap
                                         4 that includes the most important components and features.</p>
                                     <div>
@@ -91,7 +89,7 @@
                                 <card class="border-0" hover shadow body-classes="py-5">
                                     <icon name="ni ni-planet" type="warning" rounded class="mb-4">
                                     </icon>
-                                    <h6 class="text-warning text-uppercase">Prepare Launch</h6>
+                                    <h6 class="text-warning text-uppercase">AirPods Pro</h6>
                                     <p class="description mt-3">Argon is a great free UI package based on Bootstrap
                                         4 that includes the most important components and features.</p>
                                     <div>
@@ -113,32 +111,30 @@
             <div class="container">
                 <div class="row row-grid align-items-center">
                     <div class="col-md-6 order-md-2">
-                        <img src="img/theme/promo-1.png" class="img-fluid floating">
+                        <img src="img/phone/iphone11_series.png" class="img-fluid floating">
                     </div>
                     <div class="col-md-6 order-md-1">
                         <div class="pr-md-5">
                             <icon name="ni ni-settings-gear-65" class="mb-5" size="lg" type="success" shadow
                                   rounded></icon>
-                            <h3>Awesome features</h3>
-                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
-                                the text and images and you're good to go.</p>
+                            <h3>iPhone 11 또는 iPhone 11 Pro. <br>고민 된다면?</h3>                           
                             <ul class="list-unstyled mt-5">
                                 <li class="py-2">
                                     <div class="d-flex align-items-center">
                                         <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
-                                        <h6 class="mb-0">Carefully crafted components</h6>
+                                        <h6 class="mb-0">카메라 기능이 가장 중요하다면</h6>
                                     </div>
                                 </li>
                                 <li class="py-2">
                                     <div class="d-flex align-items-center">
                                         <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
-                                        <h6 class="mb-0">Amazing page examples</h6>
+                                        <h6 class="mb-0">디스플레이 사이즈가 중요하다면</h6>
                                     </div>
                                 </li>
                                 <li class="py-2">
                                     <div class="d-flex align-items-center">
                                         <badge type="success" circle class="mr-3" icon="ni ni-satisfied"></badge>
-                                        <h6 class="mb-0">Super friendly support team</h6>
+                                        <h6 class="mb-0">소재와 마감이 중요하다면</h6>
                                     </div>
                                 </li>
                             </ul>
@@ -146,104 +142,97 @@
                     </div>
                 </div>
             </div>
-        </section>
-        <section class="section bg-secondary">
-            <div class="container">
-                <div class="row row-grid align-items-center">
-                    <div class="col-md-6">
-                        <div class="card bg-default shadow border-0">
-                            <img v-lazy="'img/theme/img-1-1200x1000.jpg'" class="card-img-top">
-                            <blockquote class="card-blockquote">
-                                <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95"
-                                     class="svg-bg">
-                                    <polygon points="0,52 583,95 0,95" class="fill-default"></polygon>
-                                    <polygon points="0,42 583,95 683,0 0,95" opacity=".2" class="fill-default"></polygon>
-                                </svg>
-                                <h4 class="display-3 font-weight-bold text-white">Design System</h4>
-                                <p class="lead text-italic text-white">The Arctic Ocean freezes every winter and much of
-                                    the sea-ice then thaws every summer, and that process will continue whatever
-                                    happens.</p>
-                            </blockquote>
+        </section>       
+       <section class="section section-lg">
+           <div class="container">
+               <div class="row justify-content-center iphone_series">
+                   <div class="col-lg-3">
+                       <span class="d-block text-center">
+                           <img src="/img/phone/iphone-11-pro-max-space-gray.png" alt="iphone" width="100%">
+                       </span>
+                       <h5 class="title text-center mt-3">
+                           <span class="d-block mb-1">iPhone 11 Pro</span>
+                           <small class="h6">₩1,390,000부터</small>
+                       </h5>
+                       <p class="mt-3">완전히 새로운 트리플 카메라 시스템(울트라 와이드, 와이드, 망원)</p>
+                       <p class="mt-3">최대 20시간의 배터리 사용 시간</p>
+                       <p class="mt-3">최대 수심 4m에서 최대 30분 생활 방수</p>
+                       <p class="mt-3">5.8형 또는 6.5형 Super Retina XDR 디스플레이</p>
+                       <div class="btn-wrapper">
+                            <a type="" class="btn mb-3 btn-primary" href="">
+                            구입하기
+                            </a>
+                            <a type="" class="btn mb-3 btn-white" href="">
+                            더 알아보기 &gt;
+                            </a>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="pl-md-5">
-                            <icon name="ni ni-settings" class="mb-5" size="lg" type="warning" shadow rounded></icon>
-
-                            <h3>Our customers</h3>
-                            <p class="lead">Don't let your uses guess by attaching tooltips and popoves to any element.
-                                Just make sure you enable them first via JavaScript.</p>
-                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
-                                the text and images and you're good to go.</p>
-                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
-                                the text and images and you're good to go.</p>
-                            <a href="#" class="font-weight-bold text-warning mt-5">A beautiful UI Kit for impactful
-                                websites</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="section section section-shaped my-0 overflow-hidden">
-            <div class="shape shape-style-1 bg-gradient-warning shape-skew">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <div class="container py-0">
-                <div class="row row-grid align-items-center">
-                    <div class="col-md-6 order-lg-2 ml-lg-auto">
-                        <div class="position-relative pl-md-5">
-                            <img src="img/ill/ill-2.svg" class="img-center img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-lg-6 order-lg-1">
-                        <div class="d-flex px-3">
-                            <div>
-                                <icon name="ni ni-building" size="lg" class="bg-gradient-white" color="primary" shadow
-                                      rounded></icon>
-                            </div>
-                            <div class="pl-4">
-                                <h4 class="display-3 text-white">Modern Interface</h4>
-                                <p class="text-white">The Arctic Ocean freezes every winter and much of the sea-ice then
-                                    thaws every summer, and that process will continue whatever happens.</p>
-                            </div>
-                        </div>
-                        <card shadow class="shadow-lg--hover mt-5">
-                            <div class="d-flex px-3">
-                                <div>
-                                    <icon name="ni ni-satisfied" gradient="success" color="white" shadow
-                                          rounded></icon>
-                                </div>
-                                <div class="pl-4">
-                                    <h5 class="title text-success">Awesome Support</h5>
-                                    <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws
-                                        every summer, and that process will continue whatever happens.</p>
-                                    <a href="#" class="text-success">Learn more</a>
-                                </div>
-                            </div>
-                        </card>
-                        <card shadow class="shadow-lg--hover mt-5">
-                            <div class="d-flex px-3">
-                                <div>
-                                    <icon name="ni ni-active-40" gradient="warning" color="white" shadow
-                                          rounded></icon>
-                                </div>
-                                <div class="pl-4">
-                                    <h5 class="title text-warning">Modular Components</h5>
-                                    <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws
-                                        every summer, and that process will continue whatever happens.</p>
-                                    <a href="#" class="text-warning">Learn more</a>
-                                </div>
-                            </div>
-                        </card>
-                    </div>
-                </div>
-            </div>
-        </section>
+                   </div>
+                   <div class="col-lg-3">
+                       <span class="d-block text-center">
+                           <img src="/img/phone/iphone11_purple_lrg1.png" alt="iphone" width="100%">
+                       </span>
+                       <h5 class="title text-center mt-3">
+                           <span class="d-block mb-1">iPhone 11</span>
+                           <small class="h6">₩990,000부터</small>
+                       </h5>
+                       <p class="mt-3">완전히 새로운 듀얼 카메라 시스템(울트라 와이드, 와이드)</p>
+                       <p class="mt-3">최대 17시간의 배터리 사용 시간</p>
+                       <p class="mt-3">최대 수심 2m에서 최대 30분 생활 방수</p>
+                       <p class="mt-3">6.1형 Liquid Retina HD 디스플레이</p>
+                       <div class="btn-wrapper">
+                            <a type="" class="btn mb-3 btn-primary" href="">
+                            구입하기
+                            </a>
+                            <a type="" class="btn mb-3 btn-white" href="">
+                            더 알아보기 &gt;
+                            </a>
+                        </div>                     
+                   </div>
+                   <div class="col-lg-3">
+                       <span class="d-block text-center">
+                            <img src="/img/phone/apple-iphone-xr-64gb-coral.png" alt="iphone" width="70%">
+                       </span>
+                       <h5 class="title text-center mt-3">
+                           <span class="d-block mb-1">iPhone XR</span>
+                           <small class="h6">￦850,000부터</small>
+                       </h5>
+                       <p class="mt-3">싱글 카메라 시스템(와이드)</p>
+                       <p class="mt-3">최대 16시간의 배터리 사용 시간</p>
+                       <p class="mt-3">최대 수심 1m에서 최대 30분 생활 방수</p>
+                       <p class="mt-3">6.1형 Liquid Retina HD 디스플레이</p>
+                       <div class="btn-wrapper">
+                            <a type="" class="btn mb-3 btn-primary" href="">
+                            구입하기
+                            </a>
+                            <a type="" class="btn mb-3 btn-white" href="">
+                            더 알아보기 &gt;
+                            </a>
+                        </div>                     
+                   </div>
+                   <div class="col-lg-3">
+                       <span class="d-block text-center">
+                           <img src="/img/phone/apple-iphone-8.png" alt="iphone" width="100%">
+                       </span>
+                       <h5 class="title text-center mt-3">
+                           <span class="d-block mb-1">iPhone 8</span>
+                           <small class="h6">￦640,000부터</small>
+                       </h5>
+                       <p class="mt-3">싱글(와이드) 또는 듀얼 카메라 시스템(와이드, 망원)</p>
+                       <p class="mt-3">최대 14시간의 배터리 사용 시간</p>
+                       <p class="mt-3">최대 수심 1m에서 최대 30분 생활 방수</p>
+                       <p class="mt-3">4.7형 또는 5.5형 Retina HD 디스플레이</p>
+                       <div class="btn-wrapper">
+                            <a type="" class="btn mb-3 btn-primary" href="">
+                            구입하기
+                            </a>
+                            <a type="" class="btn mb-3 btn-white" href="">
+                            더 알아보기 &gt;
+                            </a>
+                        </div>                     
+                   </div>
+               </div>
+           </div>
+       </section>
         <section class="section section-lg">
             <div class="container">
                 <div class="row justify-content-center text-center mb-lg">
